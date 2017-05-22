ab557ad9da88fcd070fed5602a1f98f04cd91bf0
Dave <dave944@interia.pl>
2017-05-22 20:24:17 +0200

    porządki c.d.

---
+++ js/filter.js
@@ -0,0 +1,172 @@
+splitTitleFromAuthorForJson = function (json) {
+    var data = JSON.parse(json);
+    var newData = splitTitleFromAuthorForObject(data);
+    return JSON.stringify(newData);
+}
+
+splitTitleFromAuthorForJsonWithPartIncluded = function (json) {
+    var data = JSON.parse(json);
+    var newData = splitTitleFromAuthorForObjectWithPart(data);
+    return JSON.stringify(newData);
+}
+
+splitTitleFromAuthorForObject = function (object) {
+    var rowlabels = [];
+
+    for (var i = 0; i < object.rowlabels.length; i++) {
+        var book = object.rowlabels[i].split("_");
+        var author = book[0];
+        var title = book[1];
+
+        var rowlabel = {
+            author: author,
+            title: title
+        }
+
+        rowlabels.push(rowlabel);
+    }
+
+    var arr;
+    var clusters;
+    var collabels;
+
+    var newData = {};
+    newData.rowlabels = rowlabels;
+
+    if (object.arr) {
+        arr = object.arr;
+        newData.arr = arr;
+    }
+
+    if (object.clusters) {
+        clusers = object.clusters;
+        newData.clusters = clusters;
+    }
+
+    if (object.collabels) {
+        collabels = object.collabels;
+        newData.collabels = collabels;
+    }
+
+    return newData;
+}
+
+splitTitleFromAuthorForObjectWithPart = function (object) {
+    var rowlabels = [];
+
+    for (var i = 0; i < object.rowlabels.length; i++) {
+        var book = object.rowlabels[i].split("_");
+        var author = book[0];
+        var title = book[1];
+
+        var rowlabel = {
+            author: author,
+            title: title
+        }
+
+        if (book.length === 3) {
+            rowlabel.part = book[2];
+        }
+
+        rowlabels.push(rowlabel);
+    }
+
+    var arr;
+    var clusters;
+    var collabels;
+
+    var newData = {};
+    newData.rowlabels = rowlabels;
+
+    if (object.arr) {
+        arr = object.arr;
+        newData.arr = arr;
+    }
+
+    if (object.clusters) {
+        clusers = object.clusters;
+        newData.clusters = clusters;
+    }
+
+    if (object.collabels) {
+        collabels = object.collabels;
+        newData.collabels = collabels;
+    }
+
+    return newData;
+}
+
+filterByBook = function (json, book) {
+    var data = JSON.parse(json);
+    var rowlabels = data.rowlabels;
+
+    var bookIndex = -1;
+
+    if (book.part) {
+        bookIndex = rowlabels.findIndex((element) => element.author === book.author && element.title === book.title && element.part === element.part);
+    } else {
+        bookIndex = rowlabels.findIndex((element) => element.author === book.author && element.title === book.title);
+    }
+    if (bookIndex === -1) {
+        return "No results";
+    }
+
+    var result = { rowlabels: rowlabels, book: rowlabels[bookIndex] };
+
+    if (data.arr) {
+        console.log("in arr");
+        result.arr = data.arr[bookIndex];
+    }
+
+    if (data.clusters) {
+        result.clusters = data.clusters[bookIndex];
+    }
+
+    if (data.collabels) {
+        result.collabels = data.collabels[bookIndex];
+    }
+
+    return JSON.stringify(result);
+
+}
+
+filterByBookAndScore = function (json, book, score) {
+    var data = JSON.parse(json);
+    var rowlabels = data.rowlabels;
+
+    var bookIndex;
+    if (book.part) {
+        bookIndex = rowlabels.findIndex((element) => element.author === book.author && element.title === book.title && element.part === element.part);
+    } else {
+        bookIndex = rowlabels.findIndex((element) => element.author === book.author && element.title === book.title);
+    }
+
+    if (bookIndex === -1) {
+        return "No results";
+    }
+
+
+    if (!data.arr) {
+        return "No data to filter";
+    }
+
+    var arr = data.arr[bookIndex];
+
+    var filteredRowlabels = [];
+    var filteredScores = [];
+
+    for (var i = 0; i < rowlabels.length; i++) {
+
+        if (arr[i] >= score) {
+
+            filteredRowlabels.push(rowlabels[i]);
+            filteredScores.push(arr[i]);
+        }
+    }
+
+    return JSON.stringify({ rowlabels: filteredRowlabels, arr: filteredScores });
+}
+
+
+
+
